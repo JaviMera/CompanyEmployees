a157898a47b8015d773db3255b1a44007ab7709d
javier <javier_mera88@hotmail.com>
2023-02-12 20:19:23 -0600

    Added array model binder for get company collection endpoint.

--- CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -1,4 +1,5 @@
-﻿using CompanyEmployees.Service.Contracts;
+﻿using CompanyEmployees.Presentation.ModelBinders;
+using CompanyEmployees.Service.Contracts;
 using CompanyEmployees.Shared.DataTransferObjects;
 using Microsoft.AspNetCore.Mvc;
 
@@ -44,7 +45,7 @@ namespace CompanyEmployees.Presentation.Controllers
         }
 
         [HttpGet("collection/({ids})", Name = "CompanyCollection")]
-        public IActionResult GetCompanyCollection(IEnumerable<Guid> ids)
+        public IActionResult GetCompanyCollection([ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
         {
             var companies = _service.CompanyService.GetByIds(ids, trackChanges: false);
 
